3163d923127db70b8dadfd1b143ab97c75934f8e
Oskar <o.wrobel2019@gmail.com>
2025-08-26 14:36:58 +0200

    Moved the tile preview code to it's separate class

--- user_interface.cpp
+++ user_interface.cpp
@@ -2,18 +2,18 @@
 #include "user_interface.hpp"
 #include "game.hpp"
 
+
+//USER INTERFACE
+
+
 UserInterface::UserInterface(Game& game)
     : game_(game),
     window_(game_.window_),
     
     kTileSpritesheet(game_.kTileSpritesheet),
     kTileResolution(8), //game_.kTileResolution doesn't work for some reason
-    kTileTypeCount(game_.kTileTypeCount),
 
-    tile_texture_(sf::Vector2u(8, 8)),
-    black_square_(sf::Vector2f(kTileResolution, kTileResolution)),
-    tile_preview_sprite_(tile_texture_),
-    tile_place_type_(1),
+    tile_preview_({window_.getSize().x - 60.f, 60.f}, kTileResolution, game_.kTileTypeCount),
 
     my_circle_(6)
 {
@@ -21,26 +21,16 @@ UserInterface::UserInterface(Game& game)
     my_circle_.setOrigin({my_circle_.getRadius(), my_circle_.getRadius()});
     my_circle_.setPosition({(30) * 1.f, (30) * 1.f});
 
-    black_square_.setFillColor(sf::Color(0, 0, 0, 200));
-    black_square_.setOrigin({8/2.f, 8/2.f});
-    black_square_.setPosition({window_.getSize().x * 0.92f, window_.getSize().y * 0.10f});
-    black_square_.setScale(sf::Vector2f(10.f, 10.f));
-
-    game_.SetTileSpriteTexture(tile_preview_sprite_, kTileSpritesheet, kTileResolution, tile_place_type_);
-    tile_preview_sprite_.setOrigin({kTileResolution/2.f, kTileResolution/2.f});
-    tile_preview_sprite_.setPosition({window_.getSize().x * 0.92f, window_.getSize().y * 0.10f});
-    tile_preview_sprite_.setColor(sf::Color(255, 255, 255, 200));
-    tile_preview_sprite_.setScale(sf::Vector2f(10.f, 10.f));
+    game_.SetTileSpriteTexture(tile_preview_.GetSprite(), kTileSpritesheet, kTileResolution, tile_preview_.tile_type_);
 }
+
 
 void UserInterface::NextTilePlaceType() {
-    tile_place_type_++;
-    if (tile_place_type_ >= kTileTypeCount) {
-            tile_place_type_ = 1;
-        }
-    game_.SetTileSpriteTexture(tile_preview_sprite_, kTileSpritesheet, kTileResolution, tile_place_type_);
+    tile_preview_.NextTileType();
+    game_.SetTileSpriteTexture(tile_preview_.GetSprite(), kTileSpritesheet, kTileResolution, tile_preview_.tile_type_);
     return;
 }
+
 
 void UserInterface::Update() {
     my_circle_.setFillColor(sf::Color::Blue);
@@ -48,9 +38,54 @@ void UserInterface::Update() {
     return;
 }
 
+
 void UserInterface::Draw() {
     window_.draw(my_circle_);
-    window_.draw(black_square_);
-    window_.draw(tile_preview_sprite_);
+    window_.draw(tile_preview_.GetBackground());
+    window_.draw(tile_preview_.GetSprite());
     return;
 }
+
+
+// TILE PREVIEW
+
+
+TilePreview::TilePreview(sf::Vector2f position, int tile_resolution, int tile_type_count) 
+    : kTileResolution(tile_resolution), 
+    kTileTypeCount(tile_type_count), 
+
+    position_(position),
+
+    tile_texture_(sf::Vector2u(tile_resolution, tile_resolution)),
+    sprite_(tile_texture_),
+    background_(sf::Vector2f(kTileResolution, kTileResolution)),
+
+    tile_type_(1)
+
+{
+
+    background_.setFillColor(sf::Color(0, 0, 0, 200));
+    background_.setOrigin({8/2.f, 8/2.f});
+    background_.setPosition(position_);
+    background_.setScale(sf::Vector2f(10.f, 10.f));
+
+    sprite_.setOrigin({kTileResolution/2.f, kTileResolution/2.f});
+    sprite_.setPosition(position_);
+    sprite_.setColor(sf::Color(255, 255, 255, 200));
+    sprite_.setScale(sf::Vector2f(10.f, 10.f));
+}
+
+
+void TilePreview::NextTileType(){
+    tile_type_++;
+    if (tile_type_ >= kTileTypeCount) {tile_type_ = 1;}
+}
+
+
+sf::Texture& TilePreview::GetTexture(){return tile_texture_;}
+
+
+sf::Sprite& TilePreview::GetSprite() {return sprite_;}
+
+
+sf::RectangleShape& TilePreview::GetBackground() {return background_;}
--- user_interface.hpp
+++ user_interface.hpp
@@ -5,28 +5,44 @@
 
 class Game;
 
+class TilePreview{
+private:
+    int kTileResolution;
+    int kTileTypeCount;
+
+    sf::Vector2f position_;
+    
+    sf::Texture tile_texture_;
+    sf::Sprite sprite_;
+    sf::RectangleShape background_;
+
+public:
+    int tile_type_;
+    TilePreview(sf::Vector2f position, int tile_resolution, int tile_type_count);
+    void NextTileType();
+    sf::Texture& GetTexture();
+    sf::Sprite& GetSprite();
+    sf::RectangleShape& GetBackground();
+
+};
+
 class UserInterface {
-    private:
-        Game& game_;
-        sf::RenderWindow& window_;
-        const sf::Texture& kTileSpritesheet;
-        int kTileResolution;
+private:
+    Game& game_;
+    sf::RenderWindow& window_;
+    const sf::Texture& kTileSpritesheet;
+    int kTileResolution;
 
-        const int kTileTypeCount;
+public:
 
-        sf::Texture tile_texture_;
-        
-    public:
-        sf::CircleShape my_circle_;
-        sf::RectangleShape black_square_;
-        sf::Sprite tile_preview_sprite_;
-        int tile_place_type_;
+    TilePreview tile_preview_;
+    sf::CircleShape my_circle_;
 
-        UserInterface();
-        UserInterface(Game& game);
-        void NextTilePlaceType();
-        void Update();
-        void Draw();
+    UserInterface();
+    UserInterface(Game& game);
+    void NextTilePlaceType(); // The spritesheet texture getting function needs to be changed to move this
+    void Update();
+    void Draw();
 };
 
 #endif
